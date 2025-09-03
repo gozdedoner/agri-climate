17edf73f160716e8fb71b26ad9cd277255b83458
gozdedoner <gozde.doner.54@gmail.com>
2025-09-03 15:48:18 +0300

    feat: two-country comparison chart + map choropleth

--- src/main.js
+++ src/main.js
@@ -3,16 +3,3 @@ import "./style.css";
 import App from "./App.vue";
 import "leaflet/dist/leaflet.css";
 createApp(App).mount("#app");
-
-console.log(
-  "A lens:",
-  seriesA.value.temp.length,
-  seriesA.value.precip.length,
-  seriesA.value.agri.length
-);
-console.log(
-  "B lens:",
-  seriesB.value.temp.length,
-  seriesB.value.precip.length,
-  seriesB.value.agri.length
-);
